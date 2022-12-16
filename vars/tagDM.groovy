29e517aa4035d3049debe368e89529982e7c4d11
Aditya Bhat <aditya.bhat@nutanix.com>
2022-12-16 18:27:44 +0530

    retry dm loading if fails

--- vars/tagDM.groovy
+++ vars/tagDM.groovy
@@ -2,7 +2,19 @@ def call() {
   sh "mkdir -p /tmp/containers"
   sh "docker login -u calm -p eye2eye registry.calm.nutanix.com:5001"
   sh "cd /tmp/containers && wget http://endor.dyn.nutanix.com/GoldImages/domain_manager/docker/master/domain_manager.tar.xz"
-  sh "docker load -i /tmp/containers/domain_manager.tar.xz"
+  while (true) {
+      def loadCmd = "docker load -i /tmp/containers/domain_manager.tar.xz".execute()
+      loadCmd.consumeProcessOutput()
+      loadCmd.waitFor()
+      exitCode = loadCmd.exitValue()
+      if (exitCode == 0) {
+        println "dm loaded successfully"
+        break
+      }
+      else {
+        println "dm loading failed, retrying..."
+      }
+  }
   sh "docker rmi domain_manager"
 //   sh "docker tag domain_manager:latest registry.calm.nutanix.com:5001/domain_manager/nightly:latest "
 //   sh "docker push registry.calm.nutanix.com:5001/domain_manager/nightly"
