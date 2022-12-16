8518c98179e4e2f223e37bf77e1d2a13792f0425
Aditya Bhat <aditya.bhat@nutanix.com>
2022-12-16 18:31:38 +0530

    update tagDM

--- vars/tagDM.groovy
+++ vars/tagDM.groovy
@@ -3,7 +3,7 @@ def call() {
   sh "docker login -u calm -p eye2eye registry.calm.nutanix.com:5001"
   sh "cd /tmp/containers && wget http://endor.dyn.nutanix.com/GoldImages/domain_manager/docker/master/domain_manager.tar.xz"
   while (true) {
-      def loadCmd = "docker load -i /tmp/containers/domain_manager.tar.xz".execute()
+      def loadCmd = "docker load -i /tmp/containers/domain_manager.tar.xz".execute([], tmpDir)
       loadCmd.consumeProcessOutput()
       loadCmd.waitFor()
       exitCode = loadCmd.exitValue()
@@ -16,6 +16,7 @@ def call() {
       }
   }
   sh "docker rmi domain_manager"
+  sh "rm /tmp/containers/domain_manager.tar.xz"
 //   sh "docker tag domain_manager:latest registry.calm.nutanix.com:5001/domain_manager/nightly:latest "
 //   sh "docker push registry.calm.nutanix.com:5001/domain_manager/nightly"
 }
