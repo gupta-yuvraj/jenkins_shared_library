56d159e38c25f629eb44dbf96eba9f99ebaf863e
Aditya Bhat <aditya.bhat@nutanix.com>
2022-12-16 17:37:55 +0530

    update testExecute

--- vars/testExecute.groovy
+++ vars/testExecute.groovy
@@ -1,7 +1,19 @@
 #!/usr/bin/env groovy
 
 def call(){
-    def runCmd = "sleep 10".execute()
-    runCmd.waitFor()
-    println "command exit code " + runCmd.exitValue()
+    String cmds = [
+        "date && false",
+        "uptime && false",
+        "echo hello",
+        "ls -l && false"
+    ]
+   for (int i = 0; i < cmds.size(); i++)
+        def runCmd = cmds[i].execute()
+        runCmd.waitFor()
+        def exitCode = runCmd.exitValue()
+        println "command exit code " + exitCode
+        if (exitCode == 0) {
+            println "exit code is 0, exiting loop"
+            break
+        }
 }
