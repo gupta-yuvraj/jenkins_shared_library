eb5fdf9854e72da19256e59f4a41f312eb2e74a7
Aditya Bhat <aditya.bhat@nutanix.com>
2022-12-16 19:01:42 +0530

    update tagDM

--- vars/tagDM.groovy
+++ vars/tagDM.groovy
@@ -2,19 +2,31 @@ def call() {
   sh "mkdir -p /tmp/containers"
   sh "docker login -u calm -p eye2eye registry.calm.nutanix.com:5001"
   sh "cd /tmp/containers && wget http://endor.dyn.nutanix.com/GoldImages/domain_manager/docker/master/domain_manager.tar.xz"
+  // while (true) {
+  //     def sout = new StringBuilder(), serr = new StringBuilder()
+  //     def loadCmd = "docker load -i /tmp/containers/domain_manager.tar.xz".execute()
+  //     loadCmd.consumeProcessOutput(sout, serr)
+  //     loadCmd.waitFor()
+  //     exitCode = loadCmd.exitValue()
+  //     if (exitCode == 0) {
+  //       println "dm loaded successfully"
+  //       break
+  //     }
+  //     else {
+  //       println "dm loading failed, retrying..."
+  //     }
+  // }
   while (true) {
-      def sout = new StringBuilder(), serr = new StringBuilder()
-      def loadCmd = "docker load -i /tmp/containers/domain_manager.tar.xz".execute()
-      loadCmd.consumeProcessOutput(sout, serr)
-      loadCmd.waitFor()
-      exitCode = loadCmd.exitValue()
-      if (exitCode == 0) {
-        println "dm loaded successfully"
-        break
-      }
-      else {
-        println "dm loading failed, retrying..."
-      }
+    sh "docker load -i /tmp/containers/domain_manager.tar.xz"
+    def exitCode = sh "echo $?"
+    println "exitCode -> ${exitCode}" 
+    if (exitCode == '0') {
+      println "dm loading successful"
+      break
+    }
+    else {
+      println "dm loading failed, retrying.."
+    }
   }
   sh "docker rmi domain_manager"
   sh "rm /tmp/containers/domain_manager.tar.xz"
